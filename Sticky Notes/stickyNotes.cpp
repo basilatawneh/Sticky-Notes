06d068aff62a4cd0cb53512553dd0d2fecfcc5b4
BasilAtawneh <basil.atawneh.1998@gmail.com>
2019-07-24 01:31:01 +0300

    put the program interface

--- Sticky Notes/stickyNotes.cpp
+++ Sticky Notes/stickyNotes.cpp
@@ -47,5 +47,17 @@ void ViewNotesForAUser(){
 }
 int main(){
 //TODO combine the functions
+	int choise;
+	while(true){
+		cout<<"Welcome to the brand new “Sticky Notes”!\n";
+		cout<<"Here is the list of operation this program offers:\n";
+		cout<<"1- Add new user\n";
+		cout<<"2- Add new note\n";
+		cout<<"3- View notes for a specific user\n";
+		cout<<"4- Exit\n";
+		cout<<"Enter your operation : ";
+		cin>>choise;
+	
+	}
 return 0;
 }
