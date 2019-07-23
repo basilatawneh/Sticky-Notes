34da8df3e14aa8c77e4afa58f14ac3a33fa34a65
BasilAtawneh <basil.atawneh.1998@gmail.com>
2019-07-23 15:00:48 +0300

    initial the project

---
+++ Sticky Notes/stickyNotes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+ifstream file;
+
+void addNewUser(){
+//TODO write the function
+
+}
+void addNewnote(){
+//TODO write the function
+
+}
+void ViewNotesForAUser(){
+//TODO write the function
+}
+int main(){
+//TODO combine the functions
+return 0;
+}
